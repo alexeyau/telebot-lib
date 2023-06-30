a2bd86f36a08f4e4855d5873d1b35bbd7578d51a
Aleksey AU <your_email@abc.example>
2023-06-30 20:46:36 +0400

    fix require bug

--- src/services/localStorage.js
+++ src/services/localStorage.js
@@ -1,8 +1,8 @@
-export const getStorageItem = (key) => {
+const getStorageItem = (key) => {
   return localStorage.getItem(key);
 };
 
-export const setStorageItem = (key, value) => {
+const setStorageItem = (key, value) => {
   localStorage.setItem(key, value);
 };
 
